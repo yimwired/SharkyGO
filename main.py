a25fcc9eaa13e5dc872c4308562707f296a64561
ณภัทร ปานเกลี้ยง <dchnphathrp@gmail.com>
2025-02-25 12:27:00 +0700

    add 1st level background

--- main.py
+++ main.py
@@ -7,10 +7,16 @@ from kivy.vector import Vector
 from kivy.core.audio import SoundLoader
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.clock import Clock
+from kivy.core.window import Window
 import random
 
 class MenuScreen(Screen):
     def on_enter(self):
+        # Get screen size
+        screen_width = Window.width
+        screen_height = Window.height
+        print(f"Screen Width: {screen_width}, Screen Height: {screen_height}")
+
         if not hasattr(self, 'background_music'):
             self.background_music = SoundLoader.load('assets/sounds/BackgroundMermaid.mp3')
    #         if self.background_music:
